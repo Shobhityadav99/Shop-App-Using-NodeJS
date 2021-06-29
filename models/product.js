60b5497b7c89dd4c3958d31c34a5281db8315668
Shobhit Yadav <shobhit999000@gmail.com>
2021-06-29 12:30:02 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(
+        process.mainModule.filename),
+        'data',
+        'products.json');
+const fetchDatafromFile = cb => {
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        fetchDatafromFile(products => {
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err);
+            })
+        });
+    }
+
+    static fetchAll(cb) {
+        fetchDatafromFile(cb);
+    }
+}
