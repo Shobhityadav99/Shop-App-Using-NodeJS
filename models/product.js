ed96fb251e5c46b59113e2f4a317a229cc774e01
Shobhit Yadav <shobhit999000@gmail.com>
2021-06-29 14:00:56 +0530

    More fields added in form

--- models/product.js
+++ models/product.js
@@ -18,6 +18,9 @@ const fetchDatafromFile = cb => {
 module.exports = class Product {
     constructor(title,imageUrl,description,price) {
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
