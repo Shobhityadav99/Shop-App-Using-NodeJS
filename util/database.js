f41d4841856272bed9b2cccf7e4ccd3d1a00448b
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-07 15:57:25 +0530

    Sequelize configured

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'shobhit', { dialect: 'mysql', host: 'localhost' });
+const sequelize = new Sequelize('node-complete', 'root', 'shobhit', { dialect: 'mysql', host: 'localhost',port: '6900' });
 
 module.exports = sequelize;
