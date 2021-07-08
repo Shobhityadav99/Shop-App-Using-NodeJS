317af7df48b157b763add2b98fab204e7e05baf8
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-08 18:26:44 +0530

    Setting up mongo db

--- util/database.js
+++ util/database.js
@@ -1,5 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-project', 'root', '', { dialect: 'mysql', host: 'localhost' });
+const MongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://shobhit:shobhit@cluster0.fqruj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('connected!')
+            callback(client);
+        })
+        .catch(err => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = MongoConnect;
