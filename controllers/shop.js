75df687aa9096ec82959b60048af346fb446d09e
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-08 13:52:11 +0530

    Adding to cart done

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,27 +65,35 @@ exports.postCartDelete = (req,res,next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
-      .then(cart => {
-        fetchedCart = cart;
-        return cart.getProducts({where : {id: prodId}});
-      }).then(products => {
-        let product;
-        if(products.length > 0){
-          product = products[0];
-        }
-        let newQuantity = 1;
-        if(product){
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
 
-        }
-        return Product.findByPk(prodId)
-          .then(product => {
-            return fetchedCart.addProduct(product,{through: {quantity: newQuantity}});
-          }).then(() => {
-            // res.redirect('/cart');
-          })
-      }).catch(err => console.log(err));
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findById(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
