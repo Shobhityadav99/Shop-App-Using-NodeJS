7f62b263a1d34fd037aff5728d1d37d16d672769
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-12 01:31:08 +0530

    Adding product bug fixed

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongoDb.ObjectId(this.id);
+    this._id = id ? new mongoDb.ObjectId(this.id) : null;
   }
 
   save() {
