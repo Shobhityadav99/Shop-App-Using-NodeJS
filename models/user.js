edf5eaf8391ee6a5283e95abd767aaf23f26dda9
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-12 20:54:09 +0530

    Deleting added in cart

--- models/user.js
+++ models/user.js
@@ -37,6 +37,14 @@ UserSchema.methods.addToCart = function(product) {
     return this.save();
 }
 
+UserSchema.methods.removeFromCart = function(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
+}
+
 module.exports = mongoose.model('User',UserSchema);
 
 // const mongoDb = require('mongodb');
