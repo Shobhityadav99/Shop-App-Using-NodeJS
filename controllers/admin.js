1899a05015a2daf7d98136c03b83e8f254bec85d
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-12 19:00:50 +0530

    Adding mongoose everywhere

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl,null,req.user._id);
+  const product = new Product({title: title, price: price, description: description, imageUrl: imageUrl});
   product
     .save()
     .then(result => {
@@ -53,13 +53,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new
-    Product(updatedTitle,
-      updatedPrice,
-      updatedDesc,
-      updatedImageUrl,
-      prodId);
-  product.save()
+  Product.findById(prodId).then(product => {
+    product.title=updatedTitle;
+    product.price=updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    product.save()
+  })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -68,7 +68,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
