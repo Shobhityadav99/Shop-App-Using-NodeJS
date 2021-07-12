42dd2a6008e45dc5616af931316853188c1a3d37
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-12 20:37:32 +0530

    User and product linked

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({title: title, price: price, description: description, imageUrl: imageUrl});
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId: req.user._id
+  });
   product
     .save()
     .then(result => {
@@ -54,8 +60,8 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   Product.findById(prodId).then(product => {
-    product.title=updatedTitle;
-    product.price=updatedPrice;
+    product.title = updatedTitle;
+    product.price = updatedPrice;
     product.imageUrl = updatedImageUrl;
     product.description = updatedDesc;
     product.save()
