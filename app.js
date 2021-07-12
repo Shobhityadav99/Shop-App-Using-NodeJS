a873cde5fffc992dda13c079f8b8e8bc97d405d9
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-12 18:38:05 +0530

    Working with mongoose

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("60ec005b225f40429655d85c")
     .then(user => {
-      req.user = new User(user.name,user.email,user.cart,user._id);
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +32,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect('mongodb+srv://shobhit:shobhit@cluster0.fqruj.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result => {
+    app.listen(3000);
+  }).catch(err => {
+    console.log(err);
+  })
