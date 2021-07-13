f41692489370f4f5ac36c5abacf5c58e84b16762
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-13 14:09:33 +0530

    Sign up working

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,8 +8,16 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById("60ec58b7790d49104cd1446a")
+  User.findById('60ec58b7790d49104cd1446a')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
@@ -19,6 +27,27 @@ exports.postLogin = (req, res, next) => {
       });
     })
     .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({email: email})
+  .then(userDoc => {
+    if(userDoc){
+      return res.redirect('/signup');
+    }
+    const user = new User({
+      email: email,
+      password: password,
+      cart: {items: []}
+    });
+    return user.save();
+  }).then(result => {
+    res.redirect('/login');
+  })
+  .catch(err => console.log(err))
 };
 
 exports.postLogout = (req, res, next) => {
