646ebf714b642bf104d4ce5d29e06ea281f232d7
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-14 14:03:08 +0530

    Email validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const User = require('../models/user');
 const nodemailer = require('nodemailer');
 const sendGrid = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const transport = nodemailer.createTransport(sendGrid({
   auth: {
@@ -73,6 +74,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors.array());
+    return res.status('422').render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
