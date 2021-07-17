74bdc1dca4e89b895fcd552163fe0258de528409
Shobhityadav99 <shobhit999000@gmail.com>
2021-07-17 14:06:29 +0530

    Reading pdf and displaying done

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
+const pdfkit = require('pdfkit');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -146,3 +149,32 @@ exports.getOrders = (req, res, next) => {
       return next(error);
     });
 };
+
+exports.getInvoice = (req,res,next) => {
+  const orderId = req.params.orderId;
+  Order.findById(orderId).then(
+    order => {
+      if(!order){
+        return next(new Error('No order found'));
+      }
+      if(order.user.userId.toString() !== req.user._id.toString()){
+        return next(new Error("Unauthorised"));
+      }
+      const invoiceName = 'invoice-'+orderId+".pdf";
+      const invoicePath = path.join('data','invoices'+invoiceName);
+      // fs.readFile(invoicePath, (err,data) => {
+      //   if(err){
+      //     return next(err);
+      //   }
+      //   res.setHeader('Content-Type','application/pdf');
+      //   res.setHeader('Content-Disposition','attachment; filename="' + invoiceName+ '"');
+      //   res.send(data);
+      // })
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader('Content-Type','application/pdf');
+        res.setHeader('Content-Disposition','attachment; filename="' + invoiceName+ '"');
+        res.send(data);
+      file.pipe(res);
+    }
+  ).catch(err => next(err));
+}
